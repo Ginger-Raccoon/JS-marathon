544705b7e2d64151a610f2a57a9d3f573f262d99
Ilya Kosmachev <kosmachev.ie@gmail.com>
2020-09-24 22:16:10 +0300

    first task interactive

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function getRow(firstRow, secondRow) {
+const form = document.forms.form;
+
+function getRow(firstRow, secondRow, symbol) {
+
   function getSymbol(row, symbol) {
   let result = 0;
     for (let i = 0; i < row.length; i++) {
@@ -8,9 +11,18 @@ function getRow(firstRow, secondRow) {
     }
     return result
   }
-  if (getSymbol(firstRow, 'a') > getSymbol(secondRow, 'a')) {
-    return firstRow
+
+  if (getSymbol(firstRow, symbol) > getSymbol(secondRow, symbol)) {
+    return alert(firstRow)
   } else {
-    return secondRow
+    return alert(secondRow)
   }
 }
+
+document.querySelector('.button').addEventListener('click', function(event) {
+  event.preventDefault();
+  const firstRow = form.elements.firstString.value
+  const secondRow = form.elements.secondString.value
+  const symbol = form.elements.symbol.value
+  getRow(firstRow, secondRow, symbol);
+})
